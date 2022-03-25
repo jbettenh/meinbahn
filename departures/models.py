bf97a9c6d153d3da0723cec4e44796877b4e8f0e
Joseph Bettenhausen <21023218+jbettenh@users.noreply.github.com>
2022-03-25 23:53:46 +0100

    Renamed model from Departures to Departure

--- departures/models.py
+++ departures/models.py
@@ -3,7 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-class Departures(models.Model):
+class Departure(models.Model):
     id = models.AutoField(primary_key=True)
     line = models.CharField(max_length=5, default='###')
     destination = models.CharField(max_length=50, default='')
