0437cfc0990427e2cf70b42adf89e2f9188e1a49
Joseph Bettenhausen <21023218+jbettenh@users.noreply.github.com>
2022-03-26 07:16:17 +0100

    Update admin site

--- departures/admin.py
+++ departures/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-
 from .models import Departure
 
-admin.site.register(Departure)
+
+class DepartureAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Line',                      {'fields': ['line']}),
+        ('Destination',               {'fields': ['destination']}),
+        ('Date information', {'fields': ['next_train_time'], 'classes': ['collapse']}),
+    ]
+    list_display = ('line','destination','next_train_time')
+
+
+admin.site.register(Departure, DepartureAdmin)
