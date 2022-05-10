4b40037ea63b25d3c22a8adaceed8d52949de130
Joseph Bettenhausen <21023218+jbettenh@users.noreply.github.com>
2022-05-10 22:11:17 +0200

    Added test to check data passed to template

--- departures/tests.py
+++ departures/tests.py
@@ -2,7 +2,7 @@ from urllib import response
 from django.test import TestCase
 from departures.models import Departure
 
-class IndexPageTest(TestCase):
+class DeparturesPageTest(TestCase):
     def test_uses_index_template(self):
         response = self.client.get('/departures/')
 
--- routes/tests.py
+++ routes/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from routes.models import Route
 
 
-class HomePageRouteTest(TestCase):
+class RoutePageTest(TestCase):
     def test_uses_routes_template(self):
         response = self.client.get('/routes/')
 
@@ -14,9 +14,13 @@ class HomePageRouteTest(TestCase):
 #Django ORM Tests
 class RouteModelTest(TestCase):
     def setUp(self):
-        Route.objects.create(name='test1')
+        Route.objects.create(stop_id=1, direction='North',name='test1')
 
     def test_route_created(self):
         test_name = Route.objects.get(name='test1')
-        self.assertEqual(test_name.name, 'test1')
-        
+        self.assertEqual(test_name.stop_id, '1')
+    
+    def test_passes_correct_route_to_template(self):
+        correct_route = Route.objects.filter(name='test1')
+        response = self.client.get('/routes/')
+        self.assertQuerysetEqual(response.context['route_list'], correct_route)
