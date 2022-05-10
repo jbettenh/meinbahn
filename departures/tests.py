711c944cfdfb15c77bcc2718073a07a9925b036e
Joseph Bettenhausen <21023218+jbettenh@users.noreply.github.com>
2022-05-10 21:45:26 +0200

    Added departures template test

--- departures/tests.py
+++ departures/tests.py
@@ -1,3 +1,10 @@
+from urllib import response
 from django.test import TestCase
+from departures.models import Departure
 
-# Create your tests here.
+class IndexPageTest(TestCase):
+    def test_uses_index_template(self):
+        response = self.client.get('/departures/')
+
+        self.assertTemplateUsed(response, 'departures/index.html')
+
