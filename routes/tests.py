ededb30cd8110f62fe962cfd3760495c9cd29fc9
Joseph Bettenhausen <21023218+jbettenh@users.noreply.github.com>
2022-05-10 21:46:29 +0200

    Added routes template test

--- routes/tests.py
+++ routes/tests.py
@@ -1,3 +1,22 @@
+from cgi import test
+from urllib import response
 from django.test import TestCase
+from routes.models import Route
 
-# Create your tests here.
+
+class HomePageRouteTest(TestCase):
+    def test_uses_routes_template(self):
+        response = self.client.get('/routes/')
+
+        self.assertTemplateUsed(response, 'routes/routes.html')
+
+
+#Django ORM Tests
+class RouteModelTest(TestCase):
+    def setUp(self):
+        Route.objects.create(name='test1')
+
+    def test_route_created(self):
+        test_name = Route.objects.get(name='test1')
+        self.assertEqual(test_name.name, 'test1')
+        
